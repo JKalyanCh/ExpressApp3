67c1d19ee6c912571aae51b98119b6d5da11d0b2
JKalyanCh <mr.kalyanedu@gmail.com>
2025-07-17 19:24:25 +0530

    new update

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-var express = require('express')
+var express = require("express");
+var app = express();
+var tiffins = require("./tiffins.json");
+var thalies = require("./lunch.json");
+
+app.get("/", function (req, res) {
+  console.log("Request is received");
+  res.send("Response is sent");
+});
+
+app.get("/getTiffinsByName/:x", function (req, res) {
+  console.log(req.params);
+  var itom = tiffins.find((t) => {
+    if (t.name.toUpperCase() === req.params.x.toUpperCase()) {
+      return true;
+    }
+  });
+  res.send(itom);
+});
+
+app.get("/getLunchByName/:x", function (req, res) {
+  var lunch = thalies.find((t) => {
+    if (t.name.toUpperCase() === req.params.x.toUpperCase()) {
+      return true;
+    }
+  });
+  res.send(lunch);
+});
+
+app.listen(4500, function () {
+  console.log("The server is running on 4500");
+});
